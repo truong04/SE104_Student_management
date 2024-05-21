f732f7f9f5274c5c8a03eee18f8be48069135570
tanphn <tanphn2004@gmail.com>
2024-05-21 08:46:54 +0700

    tiep nhan hoc sinh

---
+++ src_code/WindowsFormsApp2/conection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace WindowsFormsApp2
+{
+    internal class conection
+    {
+        private static string stringConnection = @"Data Source=LAPTOP-P0OHVEG9;Initial Catalog=QLHS;Integrated Security=True;Encrypt=False";
+        public static SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection(stringConnection);
+        }
+
+    }
+}
